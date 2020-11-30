96761232dcf4cce2eb51af3294aee9bc06c0af32
Neil Sanchez <nd.sanchezq@gmail.com>
2020-11-30 12:06:01 -0500

    receipt chart

--- src/screens/ReceiptScreen/index.tsx
+++ src/screens/ReceiptScreen/index.tsx
@@ -41,14 +41,25 @@ const ReceiptScreen = () => {
           shadowOpacity: 0.25,
           shadowRadius: 3.84,
           elevation: 5,
-          height: 220,
+          height: 330,
           width: WWidth-40,
         }}
       >
         <View style={{flex: 1, justifyContent: 'center'}}>
           <View style={{paddingHorizontal: 20}}>
-            <Text style={{fontFamily: 'Manrope_400Regular', color: '#555'}}>Cifras de Hoy</Text>
+            <Text style={{fontFamily: 'Manrope_400Regular', color: '#555'}}>Hoy, Nov 30</Text>
           </View>
+        </View>
+
+        <View style={{ flex: 1, flexDirection: 'row', alignItems: "center", justifyContent: "center"}}>
+          <Icon type='octicon' name='primitive-dot' color='#5553F7' size={12} />
+          <Text style={{fontFamily: 'Manrope_400Regular', fontSize: 8, color: '#686354', marginRight: 20}}>  Facturado</Text>
+
+          <Icon type='octicon' name='primitive-dot' color='#FFC85B' size={12} />
+          <Text style={{fontFamily: 'Manrope_400Regular', fontSize: 8, color: '#686354', marginRight: 20}}>  Anulado</Text>
+
+          <Icon type='octicon' name='primitive-dot' color='#17D6D8' size={12} />
+          <Text style={{fontFamily: 'Manrope_400Regular', fontSize: 8, color: '#686354'}}>  Ganancia</Text>
         </View>
 
         <View style={{flex: 3, flexDirection: 'row'}}>
@@ -56,13 +67,13 @@ const ReceiptScreen = () => {
             <View style={{flex: 1}}>
               <View style={{flex: 1, paddingHorizontal: 20, justifyContent: 'flex-end'}}>
                 <View style={{flexDirection: 'row', paddingBottom: 20, alignItems: 'center'}}>
-                  <Icon type='octicon' name='primitive-dot' color='#5553F7' />
+                  <Icon type='octicon' name='primitive-dot' color='#5553F7' size={14} />
                   <Icon type='material-community' name="arrow-top-right" color='#00CF96' size={14} />
                   <Text style={{fontFamily: 'Manrope_400Regular', fontSize: 10, fontWeight: 'bold', color: '#AAA'}}> + $12.369.000,00</Text>
                 </View>
 
                 <View style={{flexDirection: 'row', paddingBottom: 20, alignItems: 'center'}}>
-                  <Icon type='octicon' name='primitive-dot' color='#FFC85B' />
+                  <Icon type='octicon' name='primitive-dot' color='#FFC85B' size={14} />
                   <Icon type='material-community' name="arrow-bottom-right" color='#EB7070' size={14} />
                   <Text style={{fontFamily: 'Manrope_400Regular', fontSize: 10, fontWeight: 'bold', color: '#AAA'}}> - $3.721.050,00</Text>
                 </View>
@@ -71,7 +82,7 @@ const ReceiptScreen = () => {
           </View>
 
           <View style={{flex: 6, justifyContent: "center", alignItems: 'center'}}>
-            <PieChart style={{ height: 150, width: 130 }} data={pieData} />
+            <PieChart style={{ height: 180, width: 160 }} data={pieData} />
             <Text
               style={{
                 fontFamily: 'Manrope_400Regular',
